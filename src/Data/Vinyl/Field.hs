bc5539c938f695a4c1f2d47a2a0acf8d8406a0e0
Index Int <vlad.z.4096@gmail.com>
2016-09-29 21:13:17 +0300

    Remove TH

--- src/Data/Vinyl/Field.hs
+++ src/Data/Vinyl/Field.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
@@ -24,15 +23,10 @@ module Data.Vinyl.Field
   , Field(..)
   , Record
   , (=:)
-  -- * Template Haskell utilities
-  , makeTag
   ) where
 
-import qualified Data.Char as Char
 import Data.Vinyl.Core
 import Data.Vinyl.Lens
-import Data.Proxy
-import qualified Language.Haskell.TH as TH
 
 -- | Bundle a tag with type.
 data Bundle k star = k :- star
@@ -53,41 +47,3 @@ type Record = Rec Field
 
 instance Show ty => Show (Field (t ':- ty)) where
   showsPrec n = showsPrec n . getField
-
-emptyDataDecl :: TH.Name -> TH.DecQ
-emptyDataDecl name = TH.dataD (return []) name [] [] []
-
-funSimple :: TH.Name -> TH.ExpQ -> TH.DecQ
-funSimple name body = TH.funD name [ TH.clause [] (TH.normalB body) [] ]
-
-proxySimple :: TH.Name -> TH.TypeQ -> TH.Q (TH.Dec, TH.Dec)
-proxySimple name ty = do
-  sig <- TH.sigD name [t| Proxy $ty |]
-  val <- funSimple name [e| Proxy |]
-  return (sig, val)
-
-tagMangle :: String -> (TH.Name, TH.Name)
-tagMangle str = (tagName, tagProxyName)
-  where
-    (strTagName, strTagProxyName) = case str of
-      []     -> ([], [])
-      (c:cs) -> (Char.toUpper c : cs, Char.toLower c : cs)
-    tagName = TH.mkName strTagName
-    tagProxyName = TH.mkName strTagProxyName
-
-mkTag :: (TH.Name, TH.Name) -> TH.DecsQ
-mkTag (tagName, tagProxyName) = do
-  tagDecl <- emptyDataDecl tagName
-  (tagProxySig, tagProxyVal) <- proxySimple tagProxyName (TH.conT tagName)
-  return [tagDecl, tagProxySig, tagProxyVal]
-
--- |
--- Creates a tag and a value-level proxy for it.
---
--- @'makeTag' \"Foo\"@ generates the following code:
---
--- > data Foo
--- > foo :: Proxy Foo
--- > foo = Proxy
-makeTag :: String -> TH.DecsQ
-makeTag = mkTag . tagMangle
