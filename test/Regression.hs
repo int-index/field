322c0ec769a8e08f6e245efad607dc32d9f28b93
Index Int <vlad.z.4096@gmail.com>
2016-09-29 22:40:38 +0300

    Fix tests

--- test/Regression.hs
+++ test/Regression.hs
@@ -3,21 +3,23 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 module Main where
 
+import Data.Proxy
 import Data.Vinyl.Field
 
-makeTag "Foo"
-makeTag "Bar"
+data Foo
+data Bar
 
 main :: IO ()
 main = do
   let
     r0 :: Record _
-    r0 = foo =: 15 <+> bar =: True
+    r0 = Proxy @Foo =: 15 <+> Proxy @Bar =: True
 
     r1 :: Num n => Record '[Foo ':- n, Bar ':- Bool]
-    r1 = foo =: 42 <+> bar =: False
+    r1 = Proxy @Foo =: 42 <+> Proxy @Bar =: False
 
   print r0
   print r1
